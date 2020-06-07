dd5eff03401a69b43c110112e82e846b7bdefabc
NoNAMe-0x16 <einokt@naver.com>
2020-06-07 20:30:50 +0900

    test_success

---
+++ app/src/main/java/com/example/opencvtest/CameraActivity.java
@@ -0,0 +1,4 @@
+package com.example.opencvtest;
+
+public class CameraActivity {
+}
--- app/src/main/java/com/example/opencvtest/MainActivity.java
+++ app/src/main/java/com/example/opencvtest/MainActivity.java
@@ -12,11 +12,17 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import android.app.AlertDialog;
+
+/*import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity;*/
 import android.view.View;
 import android.widget.ImageView;
 
@@ -202,7 +208,4 @@ public class MainActivity extends AppCompatActivity {
         });
         builder.create().show();
     }
-
-
-    public native String stringFromJNI();
 }
